f84eb8acb096ab3af6a125aa507a4f17d9489fd6
Johan F. <johan.artmat@gmail.com>
2024-05-18 16:10:26 -0500

    [CONF/UPDATE] MongoDB connection and project structure
    Configuration of connection with mongodb in the project_master database and creation of apps that make up the project structure.

--- project_master/settings.py
+++ project_master/settings.py
@@ -75,11 +75,10 @@ WSGI_APPLICATION = 'project_master.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'project_master',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
